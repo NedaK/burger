8a743e076017e3d4493f0c041639aa2757292603
NedaK <nedak26@gmail.com>
2019-04-01 09:53:56 -0700

    display working

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,9 @@
 
-
 Create database burgers_db;
 use burgers_db;
 
 Create table burgers(
     id integer auto_increment primary key,
     burger_name varchar(100) not null,
-    devoured boolean
+    devoured boolean DEFAULT false
 );
