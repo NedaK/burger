48ce0e099f44064ab617f2c89970d532630184ad
NedaK <nedak26@gmail.com>
2019-04-01 09:58:31 -0700

    finished

--- models/burger.js
+++ models/burger.js
@@ -17,12 +17,8 @@ var burger = {
       cb(res);
     });
   },
-//   delete: function(condition, cb) {
-//     orm.delete("cats", condition, function(res) {
-//       cb(res);
-//     });
-//   }
+
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller 
 module.exports = burger;
