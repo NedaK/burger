794a572a5207a7db82d195b29e482b3487a07797
NedaK <nedak26@gmail.com>
2019-04-02 17:59:19 -0700

    made db useable for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "You601Thi$$",
-  database: "burgers_db"
-});
+var connection;
+
+  if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  else{
+    connection = mysql.createConnection({
+      host: "127.0.0.1",
+      port: 3306,
+      user: "root",
+      password: "You601Thi$$",
+      database: "burgers_db"
+  });
+  
+};
 
 // Make connection.
 connection.connect(function(err) {
